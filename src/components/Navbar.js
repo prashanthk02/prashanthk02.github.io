8b7947fb79b2f55ab4e6bdfdf0868315093a3c55
prashanthk02 <prashanth.k369@gmail.com>
2022-05-31 20:58:37 -0400

    Install Mui and Create Nav

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+
+const Navbar = () =>{
+  return (
+    <div>
+      <Box padding={3}>
+        <Typography variant="h4" component="h1">
+          Prashanth Kommidi
+        </Typography>
+      </Box>
+    </div>
+  )
+}
+
+export default Navbar;
