a8f92a27974ce136947b1aa6f82b6704a7015b8c
prashanthk02 <prashanth.k369@gmail.com>
2022-06-01 20:36:43 -0400

    Add contact skeleton

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Experience from './components/Experience';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Skills />
       <Projects />
       <Experience />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+import '../styles/contact.css'
+
+const Contact = () => {
+  return (
+    <div className='contact'>
+      <h4>Contact</h4>
+    </div>
+  )
+}
+
+export default Contact;
