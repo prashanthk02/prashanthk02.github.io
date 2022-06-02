4a8b4c535aeaf261ac476e43e95488d0c43c8cb0
prashanthk02 <prashanth.k369@gmail.com>
2022-06-02 16:22:08 -0400

    Add image in about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,23 @@
-import '../styles/about.css'
+import "../styles/about.css";
+import p from "../docs/p.jpg";
 
 const About = () => {
   return (
-    <div className='about'>
+    <div className="about">
       <h3>About me</h3>
-      <img src='' alt='prashanth'/>
+      <div className="about--desc">
+        <img className="about--img" src={p} alt="prashanth" />
+        <p className="about--p">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+          when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+          It has survived not only five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets
+          containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
+          PageMaker including versions of Lorem Ipsum
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default About;
