f6910ab09130ded388240c5a6920a620f0c5db3a
prashanthk02 <prashanth.k369@gmail.com>
2022-06-01 20:27:41 -0400

    Add skills skeleton

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import About from './components/About';
+import Skills from './components/Skills';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <About />
+      <Skills />
     </div>
   );
 }
---
+++ src/components/Skills.js
@@ -0,0 +1,11 @@
+import '../styles/skills.css'
+
+const Skills = () => {
+  return (
+    <div className='skills'>
+      <h4>Skills</h4>
+    </div>
+  )
+}
+
+export default Skills;
