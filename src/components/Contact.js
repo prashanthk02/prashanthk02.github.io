ea7b11db2a45c4635580e102c357a10a53c7ad08
prashanthk02 <prashanth.k369@gmail.com>
2022-06-07 19:36:31 -0400

    Add contact deatils

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,26 @@
-import '../styles/contact.css'
+import '../styles/contact.css';
+import { FiMail } from 'react-icons/fi';
+import { FaLinkedin } from 'react-icons/fa';
+import { Link } from '@mui/material';
 
 const Contact = () => {
   return (
     <div className='contact'>
       <h2 className='neon'>Contact</h2>
+      <div className='contact--details'>
+        <Link className='contact--email' href='mailto:pkommidi@outlook.com'>
+          <FiMail className='contact--svg'/>
+          <p>pkommidi@outlook.com</p>
+        </Link>
+
+        <Link
+        href='https://www.linkedin.com/in/pkommidi/'
+        target="_blank"
+        rel="noopener"
+        >
+          <FaLinkedin className='contact--svg'/>
+        </Link>
+      </div>
     </div>
   )
 }
