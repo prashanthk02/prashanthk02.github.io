ba402a095f010fc6709b509b58fefc6b8d38a2ce
prashanthk02 <prashanth.k369@gmail.com>
2022-06-07 12:43:49 -0400

    Add projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,24 @@
+import { Link } from '@mui/material';
+import { useState } from 'react';
+import { FaGithub } from 'react-icons/fa';
 import '../styles/projects.css'
 
 const Projects = () => {
+  const [show, setShow] = useState(false);
+
   return (
     <div className='projects'>
       <h2 className='neon'>Projects</h2>
+      <div className='projects--container'>
+        <h3 className='projects--title'>What's sup</h3>
+        <button onClick={() => setShow(!show)}>{show ? '-' : '+'}</button>
+        <Link href='https://github.com/prashanthk02/whatssup' target="_blank" rel="noopener"><FaGithub /></Link>
+        <p>
+          Whats sup is a recipe finder app built using react on the frontend and express on the backend,
+          user can search for recipes by cuisine, ingredients, diet preference or
+          can plan daily meals by calorie intake.
+        </p>
+      </div>
     </div>
   )
 }
