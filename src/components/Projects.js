0457d8fb65668c62d64e897b87c107b2ee541841
prashanthk02 <prashanth.k369@gmail.com>
2022-06-07 18:52:44 -0400

    embed video

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -31,11 +31,23 @@ const Projects = () => {
           </Link>
         </div>
 
-        { show === 1 && <p>
-          Whats sup is a recipe finder app built using react on the frontend and express on the backend,
-          user can search for recipes by cuisine, ingredients, diet preference or
-          can plan daily meals by calorie intake.
-        </p>}
+        { show === 1 &&
+        <div className='projects--demo'>
+          <p>
+            Whats sup is a recipe finder app built using react on the frontend and express on the backend,
+            user can search for recipes by cuisine, ingredients, diet preference or
+            can plan daily meals by calorie intake.
+          </p>
+          <br />
+          <iframe
+          title='demo'
+          
+          src="https://www.youtube.com/embed/2mYcrqmxeUI?autoplay=1&mute=1&showinfo=0&loop=1"
+          >
+          </iframe>
+        </div>
+        }
+
       </div>
 
       <div className='projects--container'>
@@ -51,12 +63,23 @@ const Projects = () => {
           </Link>
         </div>
 
-        { show === 2 && <p>
-          Built using Express, jQuery,EJS and Twilio that allows users to
-          place an online food orders at a restaurant and notifies to pick up
-          when the order is ready for pickup via text message.
-          Project was built as a Midterm-Project as student at Lighthouse labs
-        </p>}
+        { show === 2 &&
+        <div className='projects--demo'>
+          <p>
+            Built using Express, jQuery,EJS and Twilio that allows users to
+            place an online food orders at a restaurant and notifies to pick up
+            when the order is ready for pickup via text message.
+            Project was built as a Midterm-Project as student at Lighthouse labs
+          </p>
+          <br />
+          <iframe
+          title='demo'
+          
+          src="https://www.youtube.com/embed/muibz8lYgDk?autoplay=1&mute=1&showinfo=0&loop=1"
+          >
+          </iframe>
+        </div>
+        }
       </div>
 
     </div>
