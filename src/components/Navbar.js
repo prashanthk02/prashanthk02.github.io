afceb337936f394fdd4ebc6aec66c6bb345ed98e
prashanthk02 <prashanth.k369@gmail.com>
2022-06-15 14:05:17 -0400

    add nav links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,19 @@
+import Link from "react-scroll/modules/components/Link";
+import { animateScroll } from "react-scroll/modules";
+import p from '../docs/p.jpg'
 import "../styles/navbar.css";
 
 const Navbar = ({ mode }) => {
   return (
     <div className="navbar">
-      <h1 className="fName">Prashanth Kommidi</h1>
-      <h1 className="sName">Prashanth</h1>
+      <img className="navbar--avatar" src={p} alt="img" onClick={animateScroll.scrollToTop} />
+      <div className="navbar--links">
+        <Link to='about' smooth={true} duration={500}>About</Link>
+        <Link to='skills' smooth={true} duration={500}>Skills</Link>
+        <Link to='projects' smooth={true} duration={500}>Projects</Link>
+        <Link to='experience' smooth={true} duration={500}>Experience</Link>
+        <Link to='contact' smooth={true} duration={500}>Contact</Link>
+      </div>
     </div>
   );
 };
