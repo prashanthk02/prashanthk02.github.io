2978684910b34c75cea3aee0124219bedcc388d1
prashanthk02 <prashanth.k369@gmail.com>
2022-06-20 16:51:22 -0400

    Add hammenu onmobilescreen

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,17 @@ import Link from "react-scroll/modules/components/Link";
 import { animateScroll } from "react-scroll/modules";
 import p from "../docs/p.jpg";
 import "../styles/navbar.css";
+import { HiMenuAlt4 } from "react-icons/hi";
+import { RiCloseFill } from "react-icons/ri";
+import { useState } from "react";
 
-const Navbar = ({ mode }) => {
+const Navbar = () => {
+  const [show, setShow] = useState(false);
+
+  const toggle = () => {
+    setShow(!show);
+  }
+
   return (
     <div className="navbar">
       <img
@@ -29,6 +38,32 @@ const Navbar = ({ mode }) => {
           Contact
         </Link>
       </div>
+
+      <div className="navbar--hammenu">
+        <div className="navbar--hammenu--icon">
+          {show ? <RiCloseFill onClick={() => toggle()} /> : <HiMenuAlt4 onClick={() => toggle()} />}
+        </div>
+        {show &&
+          <div className="navbar--hammenu--options">
+            <Link to="about" smooth={true} duration={500} onClick={() => toggle()}>
+              About
+            </Link>
+            <Link to="skills" smooth={true} duration={500} onClick={() => toggle()}>
+              Skills
+            </Link>
+            <Link to="projects" smooth={true} duration={500} onClick={() => toggle()}>
+              Projects
+            </Link>
+            <Link to="experience" smooth={true} duration={500} onClick={() => toggle()}>
+              Experience
+            </Link>
+            <Link to="contact" smooth={true} duration={500} onClick={() => toggle()}>
+              Contact
+            </Link>
+          </div>
+        }
+      </div>
+
     </div>
   );
 };
